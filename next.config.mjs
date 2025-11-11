a7295d598b6eeac9ec5773d50317328e284e317c
rmnddh229-design <rmnddh229@gmail.com>
2025-11-11 13:43:52 +0700

    Add files via upload

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: { appDir: true }
+  experimental: {
+    turbo: false
+  }
 };
+
 export default nextConfig;
